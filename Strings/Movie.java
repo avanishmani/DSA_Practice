fb0c3344ff7ecb2826bf215dff0dcf13e80ea7e4
avanishmani <avanishmanitripathi@gmail.com>
2023-08-21 23:50:09 +0530

    Comparable !

---
+++ Strings/Movie.java
@@ -0,0 +1,54 @@
+package Strings;
+
+import java.io.*;
+import java.util.*;
+
+// A class 'Movie' that implements Comparable
+class Movie implements Comparable<Movie>
+{
+    private double rating;
+    private String name;
+    private int year;
+
+    // Used to sort movies by year
+    public int compareTo(Movie m)
+    {
+        return this.year - m.year;
+    }
+
+    // Constructor
+    public Movie(String nm, double rt, int yr)
+    {
+        this.name = nm;
+        this.rating = rt;
+        this.year = yr;
+    }
+
+    // Getter methods for accessing private data
+    public double getRating() { return rating; }
+    public String getName()   {  return name; }
+    public int getYear()      {  return year;  }
+}
+
+// Driver class
+class Main
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Movie> list = new ArrayList<Movie>();
+        list.add(new Movie("Force Awakens", 8.3, 2015));
+        list.add(new Movie("Star Wars", 8.7, 1977));
+        list.add(new Movie("Empire Strikes Back", 8.8, 1980));
+        list.add(new Movie("Return of the Jedi", 8.4, 1983));
+
+        Collections.sort(list);
+
+        System.out.println("Movies after sorting : ");
+        for (Movie movie: list)
+        {
+            System.out.println(movie.getName() + " " +
+                    movie.getRating() + " " +
+                    movie.getYear());
+        }
+    }
+}
